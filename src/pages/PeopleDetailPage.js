ccd2938a20686d5e6a51c609431680607baeef6e
Luicil <luicilfernandes@hotmail.com>
2022-01-21 14:23:42 -0300

    aula 72

--- src/pages/PeopleDetailPage.js
+++ src/pages/PeopleDetailPage.js
@@ -1,12 +1,29 @@
 import React from "react";
-import{View, Text} from "react-native";
+import{View, Text, Image, StyleSheet} from "react-native";
 
 export default class PeopleDetailPage extends React.Component{
     render(){
+        const {people} = this.props.navigation.state.params;
+        
         return(
-            <View>
+            <View style={styles.container}>
+                <Image 
+                    style={styles.avatar} 
+                    source={{uri: people.picture.large}} />
                 <Text>Detalhes ok</Text>
             </View>
         )
     };
 };
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 15,
+    },
+    avatar: {
+        aspectRatio: 1,
+        flex: 1,
+        marginLeft: 15,
+        borderRadius: 50,
+    }
+});
