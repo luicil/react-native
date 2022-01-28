77629a559f630e6c93abd92c4e5eb1cf483b3460
Luicil <luicilfernandes@hotmail.com>
2022-01-28 17:03:44 -0300

    Aula 82

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -4,36 +4,18 @@ import PeopleListItem from "./PeopleListItem";
 
 const PeopleList = props =>{
     const {people, onPressItem} = props;
-
-/*
-    const items = people.map(person =>
-        <PeopleListItem
-            key={person.login.uuid} 
-            people={person}
-            navigateToPeopleDetail={onPressItem}
-        />
-    );
-*/
-
     return (
         <FlatList 
             style={styles.container}
             data={people}
             renderItem={({ item }) =>(
                 <PeopleListItem
-                    //key={person.login.uuid} 
                     people={item}
                     navigateToPeopleDetail={onPressItem}
                 />
             )}
             keyExtractor={item => item.login.uuid}
         />
-
-/*
-        <ScrollView style={styles.container}>
-            {items}
-        </ScrollView>
-*/
     );
 };
 
--- src/pages/PeoplePage.js
+++ src/pages/PeoplePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 import PeopleList from '../components/PeopleList';
@@ -9,32 +9,38 @@ export default class PeoplePage extends React.Component{
   constructor(props) {
     super(props);
     this.state ={
-      people:[]
+      people:[],
+      loading: false,
     };
 
   };
 
   componentDidMount(){
-    axios.get("https://randomuser.me/api?nat=br&results=150").then(
-      response =>{
-        const { results } = response.data;
-        this.setState({
-          people: results
-        });
-      }
-    ).catch(
-      response =>{
-        people: ["Falha no retorno da API."]
-      }
-    );
+
+    setTimeout(() => {
+      this.setState({loading: true});
+      axios.get("https://randomuser.me/api?nat=br&results=15").then(
+        response =>{
+          const { results } = response.data;
+          this.setState({
+            people: results,
+            loading: false,
+          });
+        }
+      ).catch(
+        response =>{
+          people: ["Falha no retorno da API."]
+        }
+      );
+        
+    }, 1500);
+
   };
 
   render(){
-
-    // this.props.navigation.navigate("PeopleDetail");
-
     return (  
       <View>
+        <ActivityIndicator size="large" color="#6ca2f7"/>
         <PeopleList
           people={this.state.people}
           onPressItem={pageParams =>{
