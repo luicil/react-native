7582b345c088285050eefe1583603271045f4008
Luicil <luicilfernandes@hotmail.com>
2022-01-28 14:28:30 -0300

    Aula 81

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,10 +1,11 @@
 import React from "react";
-import {ScrollView, Text, StyleSheet } from "react-native";
+import {FlatList, Text, StyleSheet } from "react-native";
 import PeopleListItem from "./PeopleListItem";
 
 const PeopleList = props =>{
     const {people, onPressItem} = props;
 
+/*
     const items = people.map(person =>
         <PeopleListItem
             key={person.login.uuid} 
@@ -12,11 +13,27 @@ const PeopleList = props =>{
             navigateToPeopleDetail={onPressItem}
         />
     );
+*/
 
     return (
+        <FlatList 
+            style={styles.container}
+            data={people}
+            renderItem={({ item }) =>(
+                <PeopleListItem
+                    //key={person.login.uuid} 
+                    people={item}
+                    navigateToPeopleDetail={onPressItem}
+                />
+            )}
+            keyExtractor={item => item.login.uuid}
+        />
+
+/*
         <ScrollView style={styles.container}>
             {items}
         </ScrollView>
+*/
     );
 };
 
--- src/pages/PeoplePage.js
+++ src/pages/PeoplePage.js
@@ -15,7 +15,7 @@ export default class PeoplePage extends React.Component{
   };
 
   componentDidMount(){
-    axios.get("https://randomuser.me/api?nat=br&results=20").then(
+    axios.get("https://randomuser.me/api?nat=br&results=150").then(
       response =>{
         const { results } = response.data;
         this.setState({
